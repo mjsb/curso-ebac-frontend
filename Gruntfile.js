9da83e7db40ae7dea6316a97be82a8814d2c4074
mjsb <mjsb@github.com>
2024-10-18 11:38:19 -0300

    Fix project 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,96 +16,9 @@ module.exports = function (grunt) {
                 }
             }
         }
-        // watch: {
-        //     less: {
-        //         files: ['src/styles/**/*.less'],
-        //         tasks: ['less:development']
-        //     },
-        //     html: {
-        //         files: ['src/index.html'],
-        //         tasks: ['replace:dev']
-        //     }
-        // },
-        // replace: {
-        //     dev: {
-        //         options: {
-        //             patterns: [
-        //                 {
-        //                     match: 'CSS_ADDRESS',
-        //                     replacement: './styles/main.css'
-        //                 },
-        //                 {
-        //                     match: 'JS_ADDRESS',
-        //                     replacement: '../src/scripts/main.js'
-        //                 }
-        //             ]
-        //         },
-        //         files: [
-        //             {
-        //                 expand: true,
-        //                 flatten: true,
-        //                 src: ['src/index.html'],
-        //                 dest: 'dev/'
-        //             }
-        //         ]
-        //     },
-        //     dist: {
-        //         options: {
-        //             patterns: [
-        //                 {
-        //                     match: 'CSS_ADDRESS',
-        //                     replacement: './styles/main.min.css'
-        //                 },
-        //                 {
-        //                     match: 'JS_ADDRESS',
-        //                     replacement: './scripts/main.min.js'
-        //                 }
-        //             ]
-        //         },
-        //         files: [
-        //             {
-        //                 expand: true,
-        //                 flatten: true,
-        //                 src: ['prebuild/index.html'],
-        //                 dest: 'dist/'
-        //             }
-        //         ]
-        //     }
-        // },
-        // htmlmin: {
-        //     dist: {
-        //         options: {
-        //             removeComments: true,
-        //             collapseWhitespace: true
-        //         },
-        //         files: {
-        //             'prebuild/index.html': 'src/index.html'
-        //         }
-        //     }
-        // },
-        // clean: ['prebuild'],
-        // uglify: {
-        //     target: {
-        //         files: {
-        //             'dev/scripts/main.min.js': 'src/scripts/main.js'
-        //         }
-        //     }
-        // }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less')
-    //grunt.loadNpmTasks('grunt-contrib-watch')
-    //grunt.loadNpmTasks('grunt-replace')
-    //grunt.loadNpmTasks('grunt-contrib-htmlmin')
-    //grunt.loadNpmTasks('grunt-contrib-clean')
-    //grunt.loadNpmTasks('grunt-contrib-uglify')
 
     grunt.registerTask('default', ['less:production'])
-    // grunt.registerTask('build', [
-    //     'less:production',
-    //     'htmlmin:dist',
-    //     'replace:dist',
-    //     'clean',
-    //     'uglify'
-    // ])
 }
